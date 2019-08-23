2e8b0706a28d4db42b63094588328deea9118056
Yago Ernandes <yago700@gmail.com>
2019-08-23 18:32:36 -0300

    Navigator e Telas

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,78 +1,50 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from 'react'
+import { Platform } from 'react-native'
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+} from 'react-navigation'
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from '../components/TabBarIcon'
+import HomeScreen from '../screens/HomeScreen'
+import IssuesScreen from '../screens/IssuesScreen'
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
   default: {},
-});
+})
 
 const HomeStack = createStackNavigator(
   {
     Home: HomeScreen,
   },
   config
-);
+)
 
 HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
+  title: 'GitIssues',
+}
 
-HomeStack.path = '';
+HomeStack.path = ''
 
 const LinksStack = createStackNavigator(
   {
-    Links: LinksScreen,
+    Links: IssuesScreen,
   },
   config
-);
+)
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
+LinksStack.navigationOptions = ({ navigation }) => ({
+  title: `${navigation.state.params.name}`,
+})
 
-LinksStack.path = '';
+LinksStack.path = ''
 
-const SettingsStack = createStackNavigator(
-  {
-    Settings: SettingsScreen,
-  },
-  config
-);
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
-};
-
-SettingsStack.path = '';
-
-const tabNavigator = createBottomTabNavigator({
+const tabNavigator = createStackNavigator({
   HomeStack,
   LinksStack,
-  SettingsStack,
-});
+})
 
-tabNavigator.path = '';
+tabNavigator.path = ''
 
-export default tabNavigator;
+export default tabNavigator
